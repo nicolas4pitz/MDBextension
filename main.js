4825d40296c54d8e17ed21d2192275e9725eae90
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-07-24 09:50:38 -0300

    feat: urlId and buttonEventListener

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 
 
+
 let videoListEl = document.querySelector(".actions")
+
+//Apenas para visualização de mudanças
 let header = document.querySelector("header")
+//let listVideoEl = document.getElementById("original_header")
 
-header.style.cssText = `
-  background-color: red;
-`
+console.log('Elements found:', { videoListEl, header })
 
-let newButtomEl = document.createElement("button")
-newButtomEl.textContent("Play")
-newButtomEl.style.cssText = `
+if (header) {
+  header.style.cssText = `
+    background-color: red;
+  `
+}
+
+let newButtonEl = document.createElement("button")
+newButtonEl.textContent = "Play"
+newButtonEl.style.cssText = `
   font-size: 16px;
   color: #FFFFFF;
-  background-color: #185909;
-  margin-left: 12px;
-  width: 200px;
+  background-color: #ad9718ff;
+  margin: 12px 0px 0px 8px;
+  padding: 8px;
+  border: none;
+  border-radius: 12px;
+  cursor: pointer;
 `
 
-videoListEl.prepend(newButtomEl)
 
+newButtonEl.addEventListener('dblclick', function() {
+  let pathUrl = window.location.pathname.split('/')
+  let urlId = pathUrl[2].split('-')[0]
+  console.log(urlId)
+})
+
+videoListEl.appendChild(newButtonEl)
+console.log('Button added to actions')
